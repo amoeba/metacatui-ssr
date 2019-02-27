7b04dd93d17db542c8676d996f06467cb83bb202
Bryce Mecum <petridish@gmail.com>
2019-02-26 15:49:28 -0900

    Add async/await to express

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Verify I properly made the server non-blocking
 
 const express = require('express')
+require('express-yields');
 const app = express();
 app.use(express.static(__dirname + "/src"));
 
@@ -8,16 +9,15 @@ app.use(express.static(__dirname + "/src"));
 const helpers = require('./helpers');
 const port = process.env.PORT || 3000;
 
-app.get('*', (req, res) => {
   const start = new Date();
+app.get('*', async (req, res) => {
   const path = req._parsedOriginalUrl.path;
 
-  helpers.generateResponse(path)
-    .then(response => res.send(response))
     .then(() => {
       const elapsed = Number.parseFloat((new Date() - start) / 1000).toPrecision(4)
       console.log('[' + new Date().toISOString() + '] [' + res.statusCode + '] ' + req.url + ' (' + elapsed + 's)');
-    });
+  const response = await helpers.generateResponse(path);
+  await res.send(response);
 });
 
 app.listen(port, () => console.log('MetacatUI SSR app listening on port ' + port + '!'))
